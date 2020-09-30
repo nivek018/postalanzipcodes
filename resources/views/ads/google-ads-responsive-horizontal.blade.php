3291e992afba2b745f2f40f82e60d8f9c59f16bb
Mark Anthony Co <mnoquiao@gmail.com>
2020-09-30 16:37:26 +0800

    message

--- resources/views/ads/google-ads-responsive-horizontal.blade.php
+++ resources/views/ads/google-ads-responsive-horizontal.blade.php
@@ -23,13 +23,11 @@
         <div class="cardads mobile-ads">
             
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- 2020.responsive.horizontal.ads -->
+            <!-- 2020.mobile.horizontal.ads -->
             <ins class="adsbygoogle"
-                style="display:inline-block;min-width:300px;max-width:320px;width:100%;height:50px"
+                style="display:inline-block;width:320px;height:50px"
                 data-ad-client="ca-pub-4218246550818096"
-                data-ad-slot="5265746921"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
+                data-ad-slot="9456079856"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
