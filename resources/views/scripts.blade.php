9e6fd4732e25c1ee8cef5112742c70091c9cd6dd
Mark Anthony Co <mnoquiao@gmail.com>
2020-09-30 08:11:18 +0800

    message

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -85,3 +85,15 @@ function saveSearchLocal(u_search) {
     }
 }
 </script>
+
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141529030-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-141529030-1');
+</script>
