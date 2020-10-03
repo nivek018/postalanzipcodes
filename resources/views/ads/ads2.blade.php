6e1cfb9cba828da684e60ad1f314b26f537738ef
Mark Anthony Co <mnoquiao@gmail.com>
2020-10-03 08:19:30 +0800

    message

--- resources/views/ads/ads2.blade.php
+++ resources/views/ads/ads2.blade.php
@@ -4,10 +4,12 @@
             
             <!-- Ezoic - bottom_of_page 119 (desktop,tablet) - bottom_of_page -->
             <div id="ezoic-pub-ad-placeholder-119"> 
-            
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            </div>
+            <!-- End Ezoic - bottom_of_page 119 (desktop,tablet) - bottom_of_page -->
+                        
+            <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
             <!-- 2020.responsive.horizontal.ads -->
-            <ins class="adsbygoogle"
+            <!-- <ins class="adsbygoogle"
                 style="display:inline-block;min-width:400px;max-width:970px;width:100%;height:90px"
                 data-ad-client="ca-pub-4218246550818096"
                 data-ad-slot="5265746921"
@@ -15,10 +17,7 @@
                 data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-
-            </div>
-            <!-- End Ezoic - bottom_of_page 119 (desktop,tablet) - bottom_of_page -->
+            </script> -->
 
         </div> <!-- [ .cardads ] END -->
 
@@ -28,19 +27,18 @@
             
             <!-- Ezoic - top_of_page 120 (mobile) - bottom_of_page -->
             <div id="ezoic-pub-ad-placeholder-120">
+            </div>
+            <!-- End Ezoic - top_of_page 120 (mobile) - bottom_of_page -->
 
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
             <!-- 2020.mobile.horizontal.ads -->
-            <ins class="adsbygoogle"
+            <!-- <ins class="adsbygoogle"
                 style="display:inline-block;width:320px;height:50px"
                 data-ad-client="ca-pub-4218246550818096"
                 data-ad-slot="9456079856"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-
-            </div>
-            <!-- End Ezoic - top_of_page 120 (mobile) - bottom_of_page -->
+            </script> -->
 
         </div> <!-- [ .cardads ] END -->
 
