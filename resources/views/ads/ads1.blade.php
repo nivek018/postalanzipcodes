137d216edf1e6b17bd8debeee90f96a50c038ae4
Mark Anthony Co <mnoquiao@gmail.com>
2020-10-16 19:46:48 +0800

    message

--- resources/views/ads/ads1.blade.php
+++ resources/views/ads/ads1.blade.php
@@ -5,9 +5,9 @@
             <!-- Ezoic - top_of_page 117 (deskto,tablet) - top_of_page -->
             <div id="ezoic-pub-ad-placeholder-117"> 
             
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
             <!-- 2020.responsive.horizontal.ads -->
-            <ins class="adsbygoogle"
+            <!-- <ins class="adsbygoogle"
                 style="display:inline-block;min-width:400px;max-width:970px;width:100%;height:90px"
                 data-ad-client="ca-pub-4218246550818096"
                 data-ad-slot="5265746921"
@@ -15,7 +15,7 @@
                 data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+            </script> -->
 
             </div>
             <!-- End Ezoic - top_of_page 117 (deskto,tablet) - top_of_page -->
@@ -29,15 +29,15 @@
             <!-- Ezoic - top_of_page 118 (mobile) - top_of_page -->
             <div id="ezoic-pub-ad-placeholder-118"> 
             
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> -->
             <!-- 2020.mobile.horizontal.ads -->
-            <ins class="adsbygoogle"
+            <!-- <ins class="adsbygoogle"
                 style="display:inline-block;width:320px;height:50px"
                 data-ad-client="ca-pub-4218246550818096"
                 data-ad-slot="9456079856"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
+            </script> -->
 
             </div>
             <!-- End Ezoic - top_of_page 118 (mobile) - top_of_page -->
