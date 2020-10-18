5c639469bc06fcc5c102b4b38347467e6844d1fe
Mark Anthony Co <mnoquiao@gmail.com>
2020-10-18 18:24:55 +0800

    message

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -9,6 +9,9 @@
 <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
+<!-- font awesome kit -->
+<script src="https://kit.fontawesome.com/d234582c9d.js" crossorigin="anonymous"></script>
+
 <!-- global script will run here -->
 <script>
 /* global xhr */
