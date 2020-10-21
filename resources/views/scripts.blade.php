bea14d0cd92e966852650b734f228f642cd5d8b3
Mark Anthony Co <mnoquiao@gmail.com>
2020-10-21 11:58:45 +0800

    message

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -92,13 +92,13 @@ function saveSearchLocal(u_search) {
 
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141529030-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6ZNGZV1Q47"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-141529030-1');
+  gtag('config', 'G-6ZNGZV1Q47');
 </script>
 
 <!-- Auto Ads -->
