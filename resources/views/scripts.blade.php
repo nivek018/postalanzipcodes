dac1ed8ee5cecd272b65e95ada79d1a0c57517ae
Mark Anthony Noquiao <developer@planetvibration.ph>
2022-12-01 12:27:42 +0800

    added defer on major scriptts

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -4,16 +4,16 @@
 <!--                -->
 <!--                -->
 <!-- jQuery Core 3.5.1 -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script defer src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <!-- Bootstrap 4 -->
 <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 <!-- font awesome kit -->
-<script src="https://kit.fontawesome.com/d234582c9d.js" crossorigin="anonymous"></script>
+<script defer src="https://kit.fontawesome.com/d234582c9d.js" crossorigin="anonymous"></script>
 
 <!-- global script will run here -->
-<script>
+<script defer>
 /* global xhr */
 var xhr = null;
 
@@ -25,10 +25,10 @@ if ( null !== localStorage.getItem(`searches`) ) {
 
     /* retrieve existing user's searches */
     user_searches = JSON.parse(localStorage.getItem(`searches`));
-    
+
     /*  */
     let user_search_container = document.getElementById(`user-searches`);
-    
+
     /*  */
     let searches_holder = '';
 
@@ -37,9 +37,9 @@ if ( null !== localStorage.getItem(`searches`) ) {
 
     /*  */
     user_searches.forEach( function(item) {
-        
+
         searches_holder += `<span class="badge badge-primary search-badge mr-1"><a class="text-white p-1" href="/search-result?q=` + item + `" title="` + item + `">` + item + `</a></span>`;
-        
+
     })
 
     /*  */
@@ -62,7 +62,7 @@ function saveSearchLocal(u_search) {
 
             /* retrieve existing user's searches */
             user_searches = JSON.parse(localStorage.getItem(`searches`));
-            
+
             /* array length is 5 or more remove first item of the array */
             if ( user_searches.length > 4 ) {
                 user_searches.shift();
@@ -79,7 +79,7 @@ function saveSearchLocal(u_search) {
 
             /* add user search into array */
             user_searches = [q_];
-            
+
             /* store user search */
             localStorage.setItem(`searches`, JSON.stringify(user_searches));
 
