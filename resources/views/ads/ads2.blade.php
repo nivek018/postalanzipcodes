0369f9f3cb943b4bbac751bed35d5ee2afc184f9
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-01-27 14:31:43 +0800

    test change for responsiveness

--- resources/views/ads/ads2.blade.php
+++ resources/views/ads/ads2.blade.php
@@ -4,7 +4,7 @@
     x-data='{
         init() {
             $nextTick(() => {
-                new Swiper(this.$refs.homeOfferSlider, {
+                new Swiper(this.$refs.bottomAdSlider, {
                     loop: false,
                     loopFillGroupWithBlank: true,
                     autoplay: {
@@ -42,34 +42,34 @@
     }'
     >
         <!-- Slider main container -->
-        <div x-ref="homeOfferSlider" class="swiper block relative group w-full h-auto max-h-[180px] overflow-hidden">
+        <div x-ref="bottomAdSlider" class="swiper block relative group w-full h-auto max-h-[180px] overflow-hidden">
 
             {{--slider items wrapper --}}
             <ul class="swiper-wrapper" id="offersList">
                 <li class="swiper-slide flex flex-col items-center justify-center">
                     <a href="https://shope.ee/8zWtQtYwc9" class="relative rounded-sm overflow-hidden" title="Shopee MAS MURA Home Appliances" aria-label="Shopee MAS MURA Home Appliances">
-                        <img class=" object-contain w-fit h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
+                        <img class=" object-contain w-full h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
                         src="https://cf.shopee.ph/file/758536feb7249f5b89c4a4c496764f4d" alt="" srcset="">
                     </a>
                 </li>
 
                 <li class="swiper-slide flex flex-col items-center justify-center">
                     <a href="https://shope.ee/5AKB6hKokB" class="relative rounded-sm overflow-hidden" title="Shopee Free Shipping Discount!" aria-label="Shopee Free Shipping Discount!">
-                        <img class=" object-contain w-fit h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
+                        <img class=" object-contain w-full h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
                         src="https://cf.shopee.ph/file/ph-50009109-84f115cc090d7a8d0bb71ae0da2193e4" alt="" srcset="">
                     </a>
                 </li>
 
                 <li class="swiper-slide flex flex-col items-center justify-center">
                     <a href="https://shope.ee/20N9L0BptQ" class="relative rounded-sm overflow-hidden" title="ShopeePay On Load Promos up to 10% Discount" aria-label="ShopeePay On Load Promos up to 10% Discount">
-                        <img class=" object-contain w-fit h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
+                        <img class=" object-contain w-full h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
                         src="https://cf.shopee.ph/file/3a385114fd54b4d4f46d9d751f7b8b93" alt="" srcset="">
                     </a>
                 </li>
 
                 <li class="swiper-slide flex flex-col items-center justify-center">
                     <a href="https://shope.ee/9zPQroRUWY" class="relative rounded-sm overflow-hidden" title="ShopeePay Scan All, Pay All. Win up to ₱88,888" aria-label="ShopeePay Scan All, Pay All. Win up to ₱88,888">
-                        <img class=" object-contain w-fit h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
+                        <img class=" object-contain w-full h-fit lg:max-w-[680px] lg:max-h-[180px] aspect-auto"
                         src="https://cf.shopee.ph/file/ph-50009109-bafdf36dae26c50a6772713327fb9a77" alt="" srcset="">
                     </a>
                 </li>
