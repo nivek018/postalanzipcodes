e5b8e33632801e52f29d0c7e699f2ea7a350cc31
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-03-12 23:10:26 +0800

    commented ads1 and ads2 (temporarily)

--- resources/views/pages/search-results.blade.php
+++ resources/views/pages/search-results.blade.php
@@ -5,39 +5,37 @@
 @endsection
 
 @section('content')
-<div class="relative overflow-hidden py-10 md:py-8">
+    <div class="relative overflow-hidden py-10 md:py-8">
 
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 
-        <!-- search -->
-        @include('includes.search')
-        <!--  -->
-        <!--  -->
-
-        <!-- ads -->
-        @include('ads.ads1')
-        <!--  -->
-        <!--  -->
-
-        @if ( null !== $results )
-            {{-- zip code list display --}}
-            <x-zip-list :results="$results" :page_info="$page_info" />
-        @else
-
-            <!-- no results -->
-            @include('includes.no-results')
+            <!-- search -->
+            @include('includes.search')
             <!--  -->
             <!--  -->
 
-        @endif
+            <!-- ads -->
+            {{-- @include('ads.ads1') --}}
+            <!--  -->
+            <!--  -->
 
-        <!-- ads -->
-        @include('ads.ads2')
-        <!--  -->
-        <!--  -->
+            @if (null !== $results)
+                {{-- zip code list display --}}
+                <x-zip-list :results="$results" :page_info="$page_info" />
+            @else
+                <!-- no results -->
+                @include('includes.no-results')
+                <!--  -->
+                <!--  -->
+            @endif
 
+            <!-- ads -->
+            {{-- @include('ads.ads2') --}}
+            <!--  -->
+            <!--  -->
+
+        </div>
     </div>
-</div>
 @endsection
 
 
