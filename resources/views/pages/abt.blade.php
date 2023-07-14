1692e342aaab7851b24805e2e6aae967a93a2e85
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-07-15 01:09:07 +0800

    *

--- resources/views/pages/abt.blade.php
+++ resources/views/pages/abt.blade.php
@@ -61,11 +61,10 @@
                 </p>
 
                 <p class="text-sm dark:text-gray-800 font-bold font-Inter">
-                    You may also want to check my other projects <a
-                        href="https://https://lottopcso.io/results/lotto-result-today" target="_blank"
-                        title="Visit LottoPCSO.io for daily lotto results" class="font-bold underline">LottoPCSO.io<svg
-                            class="inline-flex w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    You may also want to check my other projects <a href="https://lottopcso.io/results/lotto-result-today"
+                        target="_blank" title="Visit LottoPCSO.io for daily lotto results"
+                        class="font-bold underline">LottoPCSO.io<svg class="inline-flex w-3 h-3" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path clip-rule="evenodd" fill-rule="evenodd"
                                 d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z">
                             </path>
