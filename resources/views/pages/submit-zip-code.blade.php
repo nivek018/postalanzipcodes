518a7ca29594ca39ae01c02f102ec5d8dcbfabd0
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-07-14 17:42:01 +0800

    *

--- resources/views/pages/submit-zip-code.blade.php
+++ resources/views/pages/submit-zip-code.blade.php
@@ -68,7 +68,7 @@
                         <label for="inputZipcode"
                             class="block mb-2 text-sm font-bold text-gray-900 dark:text-white font-Inter">Zip
                             Code</label>
-                        <input type="text" name="zipcode" id="inputZipcode" minlength="4" maxlength="8"
+                        <input type="text" name="zipcode" id="inputZipcode" minlength="4" maxlength="4"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required>
                     </div>
