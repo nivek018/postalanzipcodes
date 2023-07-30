6a1509a25a308d7126c96becddc6746c123f8320
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-07-30 19:53:37 +0800

    *

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('generate:sitemap')
+        $schedule->command('sitemap:generate')
                 ->everyFiveMinutes()
                 ->timezone('Asia/Manila');
     }
