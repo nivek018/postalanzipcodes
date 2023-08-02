ad28921cb656274bd0360c9d203f738742c7d853
Mark Anthony Noquiao <mnoquiao@gmail.com>
2023-08-02 22:48:12 +0800

    *

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,14 +2,16 @@
 <html lang="en">
 
 <head>
+    <!-- Page Title -->
+    <title>@yield('title')</title>
+    <!-- Page Description -->
+    <meta name="description" content="@yield('description')">
+    <!-- Canonical -->
+    <link rel="canonical" href="@yield('canonical')" />
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title>@yield('title')</title>
-    <meta name="description" content="@yield('description')">
-    <link rel="canonical" href="@yield('canonical')" />
-
     <meta name="robots" content="index">
     <meta name="googlebot" content="index">
 
