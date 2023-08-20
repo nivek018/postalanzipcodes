c23d3a708e1435fb5e6065f07c49ee95b50f874c
nivek018 <kevs022@gmail.com>
2023-08-20 08:04:40 +0800

    updated ad codes

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -88,3 +88,53 @@
 
     gtag('config', 'G-6ZNGZV1Q47');
 </script>
+
+
+<script>
+
+var popunder=new Array()
+
+
+popunder[0]="https://bit.ly/47CdSrf"
+
+
+var width = '700';
+var height = '700';
+
+var p = 'scrollbars=no,resizable=yes,toolbar=no,' + //these are obvious variables. set "yes" or "no".
+'menubar=yes,status=yes,location=no,left=85,top=20,height=' + //the location on the user's screen
+height + ',width=' + width;
+
+var one_time=1
+
+function get_cookie(Name) {
+var search = Name + "="
+var returnvalue = "";
+if (document.cookie.length > 0) {
+offset = document.cookie.indexOf(search)
+if (offset != -1) { // if the cookie exists
+offset += search.length
+end = document.cookie.indexOf(";", offset); // set the index of beginning value
+if (end == -1) // set the index of the end of cookie value
+end = document.cookie.length;
+returnvalue=unescape(document.cookie.substring(offset, end))
+}
+}
+return returnvalue;
+}
+function loadornot(){
+if (get_cookie('popunder')==''){
+load_pop_power()
+document.cookie="popunder=yes"
+}
+}
+function load_pop_power(){
+win2=window.open(popunder[Math.floor(Math.random()*(popunder.length))],"bw",p)
+win2.blur()
+window.focus()
+}
+if (one_time==0)
+load_pop_power()
+else
+loadornot()
+</script>
