f046fbc73facc3cc6260650271aab3b45d30983a
nivek018 <kevs022@gmail.com>
2023-08-25 13:34:28 +0800

    updated ad codes

--- resources/views/components/ads-square.blade.php
+++ resources/views/components/ads-square.blade.php
@@ -1,12 +1,1 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3980043434451295"
-     crossorigin="anonymous"></script>
-<!-- postalandzipcode-square -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3980043434451295"
-     data-ad-slot="8574703673"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<a rel="nofollow" href="https://bit.ly/480p6pV"><img src="https://img.involve.asia/rpss/campaigns_banners/1610940497-2knHduBLWs3NBY6H9e4NKtKaKhnb2hKw.jpg"></a>
