0308ae172cdd0b66e4102164870d20c539911f56
nivek018 <kevs022@gmail.com>
2023-08-25 13:48:55 +0800

    updated ad codes

--- resources/views/components/ads-square.blade.php
+++ resources/views/components/ads-square.blade.php
@@ -1,1 +1,1 @@
-<a rel="nofollow" href="https://bit.ly/480p6pV" style="text-align: center;"><img src="https://img.involve.asia/rpss/campaigns_banners/1610940497-2knHduBLWs3NBY6H9e4NKtKaKhnb2hKw.jpg"></a>
+<a rel="nofollow" href="https://bit.ly/480p6pV"><img src="https://img.involve.asia/rpss/campaigns_banners/1610940497-2knHduBLWs3NBY6H9e4NKtKaKhnb2hKw.jpg" style="text-align: center;"></a>
